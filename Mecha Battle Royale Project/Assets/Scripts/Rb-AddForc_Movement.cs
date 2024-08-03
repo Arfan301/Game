d9dcaedab1de7b3f8bb758eb3e45d0d8812eafe4
Arfan Maulana <maulana.arfan301@gmail.com>
2024-08-03 20:22:14 +0700

    Optimized camera
    Added a mecha model. Now the player will always look front and rotate according to our camera movement

--- Mecha Battle Royale Project/Assets/Scripts/Rb-AddForc_Movement.cs
+++ Mecha Battle Royale Project/Assets/Scripts/Rb-AddForc_Movement.cs
@@ -1,24 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Movement : MonoBehaviour
 {
-    public float _moveSpeed = 5f;
-    public float _jumpForce = 5.0f;
     private Rigidbody _rb;
 
+    [Header("Movement")]
+    public float _moveSpeed = 5f;
+    public float _jumpForce = 5.0f;
+
+    [Header("Player Rotation")]
+    public Transform player;
+    public Transform playerObject;
+    public Transform orientation;
+    public float rotationSpeed = 30;
+
+    [Header("Camera")]
+    public Cinemachine.AxisState xAxis, yAxis;
+    [SerializeField] Transform camFollowPos;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         move();
-        
+        CameraRotation();
+        // PlayerRotation();
     }
 
     void move()
@@ -32,6 +47,35 @@ public class Movement : MonoBehaviour
         {
             _rb.AddForce(new Vector3(0, _jumpForce,0), ForceMode.Impulse);
         }
+
+    }
+
+    // may need to change this to a new script
+    void PlayerRotation()
+    {
+        Vector3 viewDirection = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDirection.normalized;
+
+        // rotate player object
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if(inputDir != Vector3.zero)
+        {
+            playerObject.forward = Vector3.Slerp(playerObject.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+        }
     }
     
+    void CameraRotation()
+    {
+        xAxis.Update(Time.deltaTime);
+        yAxis.Update(Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+        camFollowPos.localEulerAngles = new Vector3(yAxis.Value, camFollowPos.localEulerAngles.y, camFollowPos.localEulerAngles.z);
+        transform.eulerAngles = new Vector3(0, xAxis.Value, 0);
+    }
 }
